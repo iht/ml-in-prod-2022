535f371d567067dab71b24367506c243ae5561bc
Israel Herraiz <ihr@google.com>
2022-02-05 00:06:01 +0100

    Fix paths

--- bin/launch_preprocess_local.sh
+++ bin/launch_preprocess_local.sh
@@ -3,10 +3,11 @@
 # exit when any command fails
 set -e
 
-INPUT_DATA=data/
+INPUT_DATA=data/aclImdb/
 OUTPUT_DATA=output/
 
 python pipeline/preprocess_pipeline.py \
   --runner=DirectRunner \
+  --temp_location=/tmp/ \
   --data-location=$INPUT_DATA \
   --output-location=$OUTPUT_DATA
