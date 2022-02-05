b7d8774fcde123288250be9782bff256117bc053
Israel Herraiz <ihr@google.com>
2022-02-05 10:23:24 +0100

    WIP

--- bin/launch_training_local.sh
+++ bin/launch_training_local.sh
@@ -3,7 +3,7 @@ export AIP_MODEL_DIR=training-output/
 mkdir -p $AIP_MODEL_DIR
 
 python trainer/task.py \
-    --data-location=gs://ferrous-linker-339609/output/ \
-    --tft-location=gs://ferrous-linker-339609/output/transform_fn/ \
-    --batch-size=1024 \
+    --data-location=gs://class-ml-prod-2022/data/preprocessed/tf_record_with_text/ \
+    --tft-location=gs://class-ml-prod-2022/data/preprocessed/tf_record_with_text/transform_fn/ \
+    --batch-size=4096 \
     --epochs=10
