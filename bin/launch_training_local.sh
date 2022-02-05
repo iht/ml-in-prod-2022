acae74e2f735959138e3c1ac3c8361f7df27db9b
Israel Herraiz <ihr@google.com>
2022-02-05 10:26:52 +0100

    WIP

--- bin/launch_training_local.sh
+++ bin/launch_training_local.sh
@@ -5,5 +5,5 @@ mkdir -p $AIP_MODEL_DIR
 python trainer/task.py \
     --data-location=gs://class-ml-prod-2022/data/preprocessed/tf_record_with_text/ \
     --tft-location=gs://class-ml-prod-2022/data/preprocessed/tf_record_with_text/transform_fn/ \
-    --batch-size=4096 \
-    --epochs=10
+    --batch-size=64 \
+    --epochs=2
