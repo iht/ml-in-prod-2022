b7f9b8c4528d805c3cdb889e33845f2510a7a164
Israel Herraiz <ihr@google.com>
2022-02-05 11:06:46 +0100

    WIP

--- bin/launch_training_ht.sh
+++ bin/launch_training_ht.sh
@@ -2,5 +2,5 @@ gcloud ai hp-tuning-jobs create \
   --region=europe-west4 \
   --display-name=ht-my-first-model \
   --max-trial-count=56 \
-  --parallel-trial-count=8 \
+  --parallel-trial-count=2 \
   --config=./config/training_config_ht.yaml
